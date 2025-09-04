baf068b2855824aea7f3d9542d595af67142a8df
Dhia Naouali <dhia12naouali@gmail.com>
2025-09-05 00:01:26 +0100

    attribution informations

--- dinov3/loss/koleo_loss.py
+++ dinov3/loss/koleo_loss.py
@@ -1,3 +1,7 @@
+# DINOv3 in Flax/JAX
+# Ported from the original PyTorch implementation by Meta AI
+# Original repository: https://github.com/facebookresearch/dinov3
+
 from typing import Callable
 
 import jax
