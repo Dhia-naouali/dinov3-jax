56eb5e12a6bf7488b4145cdd16023edf83e5e720
Dhia Naouali <dhia12naouali@gmail.com>
2025-09-05 23:14:16 +0100

    .

---
+++ dinov3/train/__init__.py
@@ -0,0 +1,7 @@
+# DINOv3 in Flax/JAX
+# Ported from the original PyTorch implementation by Meta AI
+# Original repository: https://github.com/facebookresearch/dinov3
+
+from .multidist_meta_arch import MultiDistillationMetaArch
+from .ssl_meta_arch import SSLMetaArch
+from .train import get_args_parser, main
