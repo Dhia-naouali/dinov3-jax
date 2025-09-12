f3f398f8464c198e4a3f6365a2be08afc29a79c4
Dhia-naouali <dhia12naouali@gmail.com>
2025-09-12 20:38:59 +0100

    temp

---
+++ dinov3/distributed/__init__.py
@@ -0,0 +1,10 @@
+# DINOv3 in Flax/JAX
+# Ported from the original PyTorch implementation by Meta AI
+# Original repository: https://github.com/facebookresearch/dinov3
+
+
+# from .jax_distributed_wrapper import (
+#     ...
+# )
+
+# from .jax_distributed_primitives import gather_all_tensors, reduce_dict
---
+++ dinov3/distributed/jax_distributed_utils.py
@@ -0,0 +1,6 @@
+# JAX Distributed Utilities Skeleton
+import jax
+
+def setup_jax_distributed():
+    # TODO: Setup JAX distributed (multi-host, multi-device)
+    pass
